20d8ece06b4055fe4c3a9d27cf2ac001b4958b51
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2017-07-02 08:05:45 +0900

    スケジューラに登録

--- clock_notifier.rb
+++ clock_notifier.rb
@@ -1,9 +1,14 @@
 require 'clockwork'
-require './job_executer'
+require './b_logic_executer'
 
 include Clockwork
 
-every 10.minutes, 'gmail_post_to_slack' do
-  job_executer = JobExecuter.new(ENV['GMAIL_ACOUNT_NAME'], ENV['SLACK_WEBHOOK_URL'])
-  job_executer.gmail_post_to_slack
+# every 10.minutes, 'gmail_post_to_slack' do
+#   job_executer = JobExecuter.new(ENV['GMAIL_ACOUNT_NAME'], ENV['SLACK_WEBHOOK_URL'])
+#   job_executer.gmail_post_to_slack
+# end
+
+every 3.minutes, 'notice_kpi' do
+  blogic = BLogicExecuter.new(ENV['SLACK_WEBHOOK_URL'])
+  blogic.notice_kpi_to_slack(ENV['SPREAD_ID'], 'KPI!A1:D8')
 end
